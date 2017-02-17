423a40d83813ca123b290343b5ec1a10885c68ea
Nevidimko1 <s.mankevics@gmail.com>
2017-02-18 01:59:54 +0200

    css + removed buy/sell reducers

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,7 +1,5 @@
 import { NAME, NameReducer } from './storageService/reducers/name.reducer';
 import { MONEY, MoneyReducer } from './storageService/reducers/money.reducer';
-import { TO_BUY, ToBuyReducer } from './storageService/reducers/toBuy.reducer';
-import { TO_SELL, ToSellReducer } from './storageService/reducers/toSell.reducer';
 import { RESOURCES, ResourcesReducer } from './storageService/reducers/resources.reducer';
 import { GROUPS, GroupsReducer } from './storageService/reducers/groups.reducer';
 import { INVENTORY, InventoryReducer } from './storageService/reducers/inventory.reducer';
@@ -12,8 +10,6 @@ export * from './storageService/storage';
 export const Actions = {
     NAME,
     MONEY,
-    TO_BUY,
-    TO_SELL,
     RESOURCES,
     GROUPS,
     INVENTORY
@@ -22,8 +18,6 @@ export const Actions = {
 export const Reducers = {
     NameReducer,
     MoneyReducer,
-    ToBuyReducer,
-    ToSellReducer,
     ResourcesReducer,
     GroupsReducer,
     InventoryReducer
